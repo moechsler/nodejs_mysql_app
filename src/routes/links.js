7d0a94dbbd3fe193cdadeb6021e210fc196e8bf8
Marco Oechsler <moechsler@gmail.com>
2020-09-12 01:03:17 -0300

    agrega visualizacion de datos desde BD

--- src/routes/links.js
+++ src/routes/links.js
@@ -22,5 +22,12 @@ router.post('/add', async (req, res) =>{
 }
 );
 
+router.get('/', async (req, res) =>{
+ 
+    const  links = await pool.query('select * from links');
+    console.log(links);
+    res.render('links/list', { links });
+});
+
 
 module.exports = router;
