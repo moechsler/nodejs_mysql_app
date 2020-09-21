d62a5c48472185cf602afccf19148b0b2d28399f
Marco Oechsler <moechsler@gmail.com>
2020-09-21 20:41:08 -0300

    commit inicial iniciaSesion

---
+++ src/routes/links.js
@@ -0,0 +1,66 @@
+const { request } = require('express');
+const express = require('express');
+const router = express.Router();
+
+const pool = require('../database');
+
+
+router.get('/add', (req, res) =>{
+
+    res.render('links/add');
+});
+
+router.post('/add', async (req, res) =>{
+    const {title, url, description} = req.body;
+    const newLink = {
+        title,
+        url,
+        description
+    };
+
+    await pool.query('insert into links set ?', [newLink]);
+    req.flash('success', 'Link saved successfully');
+    res.redirect('/links');
+}
+);
+
+router.get('/', async (req, res) =>{
+ 
+    const  links = await pool.query('select * from links');
+    console.log(links);
+    res.render('links/list', { links });
+});
+
+
+router.get('/delete/:id', async (req, res) => {
+
+const {id} = req.params;
+await pool.query('DELETE FROM links WHERE ID = ?', [id]);
+req.flash('success', 'Link removed successfully');
+res.redirect('/links');
+
+});
+
+router.get('/edit/:id', async (req, res) =>{
+const {id} = req.params;
+const links = await pool.query('SELECT * from links WHERE id = ?', [id]);
+console.log(links[0]);
+res.render('links/edit', {link: links[0]} );
+});
+
+router.post('/edit/:id', async (req, res) =>{
+
+    const {id} =req.params;
+    const {title, description, url} = req.body;
+    const newLink = {
+        title,
+        description,
+        url
+    };
+    
+    await pool.query('UPDATE links SET ? where id = ?', [newLink, id]);
+    req.flash('success', 'Link updated successfully');
+    res.redirect('/links');
+});
+
+module.exports = router;
